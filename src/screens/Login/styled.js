100063cbc9625c655a05c53bfe867a8ed72d1f8f
Andrew Sushko <andrew.sushko@effective-soft.com>
2020-08-13 15:10:58 +0300

    Modify screens with styling

---
+++ src/screens/Login/styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const TitleText = styled.Text`
+  font-size: 24px;
+`;
--- src/screens/User/index.js
+++ src/screens/User/index.js
@@ -4,9 +4,15 @@ import {View, Text} from 'react-native';
 
 import {selectProfile} from 'store/auth/selectors';
 
+import * as S from './styled';
+
 const UserScreen = () => {
   const profile = useSelector(selectProfile);
-  return <View>{profile && <Text>{profile.name}</Text>}</View>;
+  return (
+    <S.Container>
+      {profile && <S.ProfileName>{profile.name}</S.ProfileName>}
+    </S.Container>
+  );
 };
 
 export default UserScreen;
