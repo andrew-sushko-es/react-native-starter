a5829c07ec75f1a437094884808eb26ff07eb0d7
Andrew Sushko <andrew.sushko@effective-soft.com>
2020-08-13 16:07:04 +0300

    Add theme folder and provide it to theme provider of styled components

--- src/App.js
+++ src/App.js
@@ -2,19 +2,24 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {SafeAreaView} from 'react-native';
 import {Provider as ReduxProvider} from 'react-redux';
+import {ThemeProvider} from 'styled-components';
 
 import configureStore from './store/configureStore';
 
 import RootScreen from './screens/Root';
+
+import theme from './theme';
 
 const store = configureStore();
 
 const App = () => {
   return (
     <ReduxProvider store={store}>
-      <SafeAreaView style={{flex: 1}}>
-        <RootScreen />
-      </SafeAreaView>
+      <ThemeProvider theme={theme}>
+        <SafeAreaView style={{flex: 1}}>
+          <RootScreen />
+        </SafeAreaView>
+      </ThemeProvider>
     </ReduxProvider>
   );
 };
