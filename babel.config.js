7390ecc38b768264e72b926ef87a2078975237e9
Andrew Sushko <andrew.sushko@effective-soft.com>
2020-08-13 12:26:49 +0300

    Add babel module resolver package and adjust absolute imports

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.js', '.json'],
+        alias: {
+          screens: './src/screens',
+          navigation: './src/navigation',
+          components: './src/components',
+          i18n: './src/i18n',
+          store: './src/store',
+          services: './src/services',
+          utils: './src/utils',
+        },
+      },
+    ],
+  ],
 };
